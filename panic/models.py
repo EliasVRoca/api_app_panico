51136522715f2ec03e9991711849db71fbebce50
EliasVR <eliasvroca@gmail.com>
2026-04-30 15:13:51 -0400

    auntemtando los digitos en l y l

--- panic/models.py
+++ panic/models.py
@@ -5,8 +5,8 @@ User = get_user_model()
 
 class PanicEvent(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='panic_events')
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    latitude = models.DecimalField(max_digits=12, decimal_places=8, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=12, decimal_places=8, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, default='Triggered')
 
